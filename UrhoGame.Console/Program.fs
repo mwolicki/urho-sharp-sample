909c2d540157584cfb05fec95ee39e5e49990c8a
Marcin Wolicki <marcin.wolicki@gmail.com>
2016-04-12 20:55:41 +0100

    fixed typo: UrhoHame -> UrhoGame

---
+++ UrhoGame.Console/Program.fs
@@ -0,0 +1,12 @@
+﻿namespace UrhoGame.Mac
+open System
+open UrhoGame.Lib
+open Urho.Desktop
+open Urho.Gui
+
+module main =
+    [<EntryPoint>]
+    let main args =
+        use app = new GameApplication()
+        app.Run()
+    
